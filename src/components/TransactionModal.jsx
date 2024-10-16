d7e747c92a79866d17d06e7459083118ccce6bfe
Asep Supriyadi <asepsupyad789@gmail.com>
2024-10-16 17:11:51 +0700

    Ngabenerkeun Form

--- src/components/TransactionModal.jsx
+++ src/components/TransactionModal.jsx
@@ -1,27 +1,26 @@
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input} from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input } from "@nextui-org/react";
 import { useState } from "react";
 
 function TransactionModal() {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
-  
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   const [formState, setformState] = useState({
-    Nama : " ",
-    NomorTelepon : " ",
-    Alamat : " ",
-    Biaya : " ",
-    Status : " ",
+    Nama: " ",
+    NomorTelepon: " ",
+    Alamat: " ",
+    Biaya: " ",
+    Status: " ",
   });
-  
+
   const handleChange = (e) => {
-    setformState({
-        ...formState,
-        [e.target.name]: e.target.value
-    })
+    setformState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
   }
 
-  const handleSubmit = (event)=>{
+  const handleSubmit = (event) => {
     event.preventDefault();
-
     console.log(formState)
   }
 
@@ -34,18 +33,28 @@ function TransactionModal() {
             <>
               <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
               <ModalBody>
-                <Input label = "Nama Pelanggan" value={formState.Nama} onChange={handleChange}/>
-                    <Input value = {formState.Nama} label = "Nama Pelanggan" className="mb-3" onChange={handleChange}/>
-                    <Input value = {formState.NomorTelepon} label = "Nomor Telepon" className="mb-3" onChange={handleChange}/>
-                    <Input value = {formState.Alamat} label = "Alamat" className="mb-3" onChange={handleChange}/>
-                    <Input value = {formState.Biaya} label = "Biaya" className="mb-3" onChange={handleChange}/>
-                    <Input onChange={handleChange} value = {formState.Status} label = "Status" className="mb-3" />
+                <Input
+                  type="text"
+                  name="Nama"
+                  value={formState.Nama}
+                  label="Nama Pelanggan"
+                  className="mb-3"
+                  onChange={handleChange}
+                />
+                <Input
+                  type="text"
+                  name="NomorTelepon"
+                  value={formState.NomorTelepon}
+                  label="Nomor Pelanggan"
+                  className="mb-3"
+                  onChange={handleChange}
+                />
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
                   Close
                 </Button>
-                <Button color="primary" onPress={onClose}  onClick={handleSubmit}>
+                <Button color="primary" onPress={onClose} onClick={handleSubmit}>
                   Action
                 </Button>
               </ModalFooter>
